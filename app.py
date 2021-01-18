be0a9eba77ab17fb3f09f98a2a183e2d29bf4901
Frenz86 <frenzy86@gmail.com>
2021-01-18 23:39:47 +0100

    ww

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from PIL import Image
+from model import predict
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+st.title("Image Classification with Pytorch")
+st.write("")
+
+file_up = st.file_uploader("Upload an image", type = ['jpg','jpeg'])
+
+if file_up is not None:
+    image = Image.open(file_up)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
+    st.write("Just a second...")
+    labels = predict(file_up)
+
+    # print out the top 5 prediction labels with scores
+    for i in labels:
+        st.write("Prediction", i[0], ",   Score: ", i[1])
